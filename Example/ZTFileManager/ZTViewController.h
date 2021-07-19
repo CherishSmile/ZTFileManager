5850e82144a6dfdcae891f0ea00c080d40d56e15
zwl <misteralvin@yeah.net>
2021-07-19 17:49:11 +0800

    Initial commit

---
+++ Example/ZTFileManager/ZTViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZTViewController.h
+//  ZTFileManager
+//
+//  Created by 1332291552@qq.com on 07/19/2021.
+//  Copyright (c) 2021 1332291552@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ZTViewController : UIViewController
+
+@end
---
+++ Example/ZTFileManager/ZTViewController.m
@@ -0,0 +1,29 @@
+//
+//  ZTViewController.m
+//  ZTFileManager
+//
+//  Created by 1332291552@qq.com on 07/19/2021.
+//  Copyright (c) 2021 1332291552@qq.com. All rights reserved.
+//
+
+#import "ZTViewController.h"
+
+@interface ZTViewController ()
+
+@end
+
+@implementation ZTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
